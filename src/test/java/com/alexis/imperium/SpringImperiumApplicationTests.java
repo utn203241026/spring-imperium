9fae61fcf8a38ac596e3d6967edfd32122a586ed
alexis <203241026@alumnos.utn.edu.mx>
2022-07-21 18:41:07 -0500

    creacion de la estructura del proyecto y de la clase model

---
+++ src/test/java/com/alexis/imperium/SpringImperiumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexis.imperium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringImperiumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
